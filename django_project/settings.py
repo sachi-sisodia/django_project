9c04cfbe0d748445feae398a83da38b626fb9d8b
Sachi Sisodia <63452638+Siya-Sachi@users.noreply.github.com>
2024-05-15 01:16:42 -0400

    implemented login and logout completely and condition for showing profile button

--- django_project/settings.py
+++ django_project/settings.py
@@ -128,3 +128,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+
+LOGOUT_REDIRECT_URL = 'logout'
+
+LOGIN_URL = 'login'
