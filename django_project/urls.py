488759044b6f0dc234f6ff2a44a681f423ae0656
Sachi Sisodia <63452638+Siya-Sachi@users.noreply.github.com>
2024-06-03 00:40:15 -0400

    start code idk before the vacay

--- django_project/urls.py
+++ django_project/urls.py
@@ -37,6 +37,16 @@ urlpatterns = [
         name="logout",
     ),
     path("profile/", user_views.profile, name="profile"),
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(template_name="users/password_reset.html"),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"),
+        name="password_reset",
+    ),
 ]
 
 if settings.DEBUG:
