f378a50c06ccdb6cbf6238d2c7998a5683ab9f95
Sachi Sisodia <63452638+Siya-Sachi@users.noreply.github.com>
2024-06-03 01:44:37 -0400

    sent password reset message using gmail directly not using os.getenviron...

--- django_project/urls.py
+++ django_project/urls.py
@@ -45,7 +45,12 @@ urlpatterns = [
     path(
         "password-reset/done/",
         auth_views.PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"),
-        name="password_reset",
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset-confirm/<uidb64>/<token>",
+        auth_views.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"),
+        name="password_reset_confirm",
     ),
 ]
 
