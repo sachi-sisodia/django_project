f2c83e909e74a3fb8108d81a97a15443492729f6
Sachi Sisodia <63452638+Siya-Sachi@users.noreply.github.com>
2024-06-06 00:51:00 -0400

    changes for cloudinary start with settings, midels, forms and a dont forget to do migrate before runserver

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from users.models import Profile
+from cloudinary.forms import CloudinaryFileField
 
 class UserRegisterForm(UserCreationForm) :
     email = forms.EmailField()
@@ -20,8 +21,17 @@ class UserUpdateForm(forms.ModelForm) :
         fields = ['username', 'email']
 
 class ProfileUpdateForm(forms.ModelForm) :
+    image = CloudinaryFileField()
+
     class Meta:
         model = Profile
-        fields = ['image']
+        # fields = ['image']
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].options={
+            'tags': 'new_image',
+        }
     
 
